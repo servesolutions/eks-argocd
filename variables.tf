1604d22b7dd2646b5e32d535122f9f6fdd558cf8
servesolutions <124923918+servesolutions@users.noreply.github.com>
2023-06-26 20:41:13 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,92 @@
+# VPC VARIABLES
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_name" {
+  type    = string
+}
+
+variable "cidr" {
+  type    = string
+  default = ""
+}
+
+variable "azs" {
+  type    = list(any)
+  default = []
+}
+
+variable "private_subnets" {
+  type    = list(any)
+  default = []
+}
+
+variable "public_subnets" {
+  type    = list(any)
+  default = []
+}
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
+  type        = bool
+}
+
+variable "single_nat_gateway" {
+  type = bool
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+}
+
+variable "enable_dns_support" {
+  type = bool
+}
+
+variable "tags" {
+  type    = map(any)
+  default = {}
+}
+
+# EKS VARIABLE
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_version" {
+  type = string
+}
+
+variable "cluster_endpoint_private_access" {
+  type = bool
+}
+
+variable "cluster_endpoint_public_access" {
+  type = bool
+}
+
+variable "cluster_addons" {
+  type = map(any)
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "enable_irsa" {
+  type = bool
+}
+
+variable "eks_managed_node_group_defaults" {
+  type = map(any)
+}
+
+variable "eks_managed_node_groups" {
+  type = map(any)
+}
+
+variable "manage_aws_auth_configmap" {
+  type = bool
+}
